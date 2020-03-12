c56792bb27114a1fea48dbd419890ec8177a6f9c
cqsd <>
2020-03-11 18:00:53 -0700

    move some stuff around

--- examples/example-crimes.py
+++ examples/example-crimes.py
@@ -1,7 +1,7 @@
 import sys
 
 from trufflehonk.jobs import trufflehog, pydriller
-from trufflehonk.utils import github_org_repos
+from trufflehonk.utils import github_org_repos, update_dict_sets
 
 
 org = 'cqsd'
@@ -12,29 +12,6 @@ secrets = []
 author_emails = dict()
 
 
-def update_dict_sets(d1, d2):
-    '''Given two dicts d1, d2 mapping keys to sets, extend the sets in d1 using
-    the sets in d2. Keys present in d2 but missing in d1 are added to d1.
-
-    Warning: d1 is mutated, d2 is not.
-
-    Example:
-
-        >>> d1 = {'a': {1, 2}}
-        >>> d2 = {'a': {1, 3}, 'b': {1}}
-        >>> update_dict_sets(d1, d2)
-        >>> d1
-        {'a': {1, 2, 3}, 'b': {1}}
-        >>> d2
-        {'a': {1, 3}, 'b': {1}}
-    '''
-    for k, v in d2.items():
-        if k in d1:
-            d1[k].update(v)
-        else:
-            d1[k] = set(v)
-
-
 for repo in repos:
     try:
         repo_url = f'https://github.com/{org}/{repo}'
@@ -43,7 +20,6 @@ for repo in repos:
         tf = trufflehog.Trufflehog(repo_url, clone_path)
         pd = pydriller.PyDriller(repo_url, clone_path)
 
-        heading = f'processing {repo_url}'
         print(f'processing {repo_url}', file=sys.stderr)
 
         tf.run()
--- trufflehonk/utils.py
+++ trufflehonk/utils.py
@@ -23,6 +23,29 @@ def flatten_dict_keys(d, parent_key='', sep='_'):
     return dict(items)
 
 
+def update_dict_sets(d1, d2):
+    '''Given two dicts d1, d2 mapping keys to sets, extend the sets in d1 using
+    the sets in d2. Keys present in d2 but missing in d1 are added to d1.
+
+    Warning: d1 is mutated, d2 is not.
+
+    Example:
+
+        >>> d1 = {'a': {1, 2}}
+        >>> d2 = {'a': {1, 3}, 'b': {1}}
+        >>> update_dict_sets(d1, d2)
+        >>> d1
+        {'a': {1, 2, 3}, 'b': {1}}
+        >>> d2
+        {'a': {1, 3}, 'b': {1}}
+    '''
+    for k, v in d2.items():
+        if k in d1:
+            d1[k].update(v)
+        else:
+            d1[k] = set(v)
+
+
 def exec_timeout(exec_args, timeout=600):
     stdout, _ = subprocess.Popen(
         exec_args,
@@ -34,7 +57,7 @@ def exec_timeout(exec_args, timeout=600):
 
 
 def github_org_repos(identifier, user=False, repo_type=None):
-    '''Yield the names of the repos in a given org/user account.
+    '''Return the names of the repos in a given org/user account.
 
     identifier (str): username or org name
     user(bool) False: if specified, tries to fetch repos for a user. Default
